a1ed6f9b8181bd6d9a913a49fdd663f1590c01ce
Ryan Hauert <ryanoffski@gmail.com>
2014-05-08 20:19:04 -0600

    Added test for demo2

--- demo/demo2_test.go
+++ demo/demo2_test.go
@@ -4,3 +4,27 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
+
+func TestWorkerPool(t *testing.T) {
+	Convey("Given a pool of five workers", t, func() {
+		pool := NewWorkerPool(5)
+
+		Convey("When given a batch of work", func() {
+			for i := 0; i < 20; i++ {
+				pool.Queue <- i
+			}
+			close(pool.Queue)
+
+			Convey("Work will be balanced", func() {
+				counts := make(map[string]int)
+				for res := range pool.Results {
+					counts[res.Worker]++
+				}
+
+				for _, v := range counts {
+					So(v, ShouldEqual, 4)
+				}
+			})
+		})
+	})
+}
