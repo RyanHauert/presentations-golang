c6cfa854600cef59d52da1115c6288b3ba559f99
Ryan Hauert <ryanoffski@gmail.com>
2014-05-08 21:10:31 -0600

    Implemented demo2

--- demo/demo2.go
+++ demo/demo2.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Result struct {
+	Worker string
+	Job    int
+}
+
+type Worker struct {
+	Name string
+}
+
+func (w Worker) DoWork(queue <-chan int, results chan<- *Result, complete chan<- bool) {
+	for i := range queue {
+		time.Sleep(100 * time.Millisecond)
+		results <- &Result{w.Name, i}
+	}
+	complete <- true
+}
+
+type WorkerPool struct {
+	workers []*Worker
+	Queue   chan<- int
+	Results <-chan *Result
+}
+
+func NewWorkerPool(workerCount int) *WorkerPool {
+	queue := make(chan int, 100)
+	results := make(chan *Result, 100)
+	complete := make(chan bool)
+	workers := make([]*Worker, workerCount)
+
+	for i := 0; i < workerCount; i++ {
+		worker := Worker{fmt.Sprintf("Worker%d", i)}
+		go worker.DoWork(queue, results, complete)
+		workers[i] = &worker
+	}
+
+	go func() {
+		for i := 0; i < workerCount; i++ {
+			<-complete
+		}
+		close(results)
+	}()
+
+	return &WorkerPool{workers, queue, results}
+}
